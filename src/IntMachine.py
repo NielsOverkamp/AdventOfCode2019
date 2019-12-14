02e6beefbee493353f62209fd40d5554dc5105de
niels <niels.overkamp@gmail.com>
2019-12-14 22:50:25 +0100

    Finish IntMachine and reimplementation of day7

--- src/IntMachine.py
+++ src/IntMachine.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Callable, MutableSequence, MutableMapping, Iterator, Iterable
 
-from itertools import chain, repeat
+from itertools import chain
 
 #
 # Example programs
@@ -47,8 +47,8 @@ def set(data, i, imm, rel_base, v):
 def tick(prog_mem, regs, inputf, outputf):
     i = regs["pc"]
     rel_base = regs["rel_base"]
-    op = prog_mem[i]
-    op, imm1, imm2, imm3 = op % 100, op // 100 % 10, op // 1000 % 10, op // 10000 % 10
+    op_code = prog_mem[i]
+    op, imm1, imm2, imm3 = op_code % 100, op_code // 100 % 10, op_code // 1000 % 10, op_code // 10000 % 10
     if op == 99:
         regs["pc"] = -1
         return
@@ -94,6 +94,8 @@ def tick(prog_mem, regs, inputf, outputf):
     elif op == 9:
         rel_base += get(prog_mem, i + 1, imm1, rel_base)
         i += 2
+    else:
+        raise Exception("Operation {} not supported".format(op_code))
     regs["pc"] = i
     regs["rel_base"] = rel_base
 
@@ -122,20 +124,56 @@ def std_out(out, machine_id=None):
         print("IntMachine {} Output: {}".format(machine_id, out))
 
 
-class IterIn:
+class InOutBuffer:
     iter: Iterator
 
-    def __init__(self, _iter: Iterable):
+    def __init__(self, _iter: Iterable = ()):
         self._iter = iter(_iter)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return next(chain(self._iter, repeat(None)))
+        return self.take_from_buffer()
 
-    def add_to_iter(self, new_iter: Iterable):
+    def take_from_buffer(self, machine_id=None):
+        try:
+            return next(self._iter)
+        except StopIteration:
+            return None
+
+    def add_all_to_buffer(self, new_iter: Iterable):
         self._iter = chain(self._iter, iter(new_iter))
+
+    def add_to_buffer(self, value, machine_id=None):
+        self._iter = chain(self._iter, iter((value,)))
+
+
+def log_in_to_std_out(inputf, print_empty_input=True):
+    def _inputf(machine_id=None, *args,  **kwargs):
+        if machine_id is None:
+            res = inputf(*args, **kwargs)
+            if res is not None or print_empty_input:
+                print("Intmachine got input: {}".format(res))
+        else:
+            res = inputf(*args, machine_id, **kwargs)
+            if res is not None or print_empty_input:
+                print("Intmachine {} got input: {}".format(machine_id, res))
+        return res
+
+    return _inputf
+
+
+def log_out_to_std_out(outputf):
+    def _outputf(out, machine_id=None, *args, **kwargs):
+        if machine_id is None:
+            outputf(out, *args, **kwargs)
+            print("IntMachine sent Output: {}".format(out))
+        else:
+            outputf(out, *args, machine_id, **kwargs)
+            print("IntMachine {} sent Output: {}".format(machine_id, out))
+
+    return _outputf
 
 
 #
@@ -158,10 +196,18 @@ def dig_str(n, d):
 
 
 def print_state(data: [int], regs: {str: int}):
-    header = "po123"
-    for d in reversed(range(0, 5)):
-        print(header[d] + " " + " ".join(map(lambda n: dig_str(n, d), data)))
-    print(" " * (2 * regs["pc"] + 2) + "^")
+    print()
+    if regs["pc"] >= 10:
+        print(" .\n .\n .\n")
+        print(" " + "\n ".join(map(str, data[regs["pc"]-10:regs["pc"]])))
+    elif regs["pc"] > 0:
+        print(" " + "\n ".join(map(str, data[:regs["pc"]])))
+    print(">{}".format(data[regs["pc"]]))
+    print(" " + "\n ".join(map(str, data[regs["pc"] + 1:regs["pc"] + 11])))
+
+    # for d in reversed(range(0, 5)):
+    #     print(header[d] + " " + " ".join(map(lambda n: dig_str(n, d), data)))
+    # print(" " * (2 * regs["pc"] + 2) + "^")
     print("{{\n{}}}".format("".join(list(map(lambda item: "\t{}: {},\n".format(*item), regs.items())))))
 
 
@@ -181,8 +227,10 @@ class Machine:
     inputf: Callable[[int], int]
     outputf: Callable[[int, int], None]
     status: Status = Status.RUNNING
+    machine_id: str
 
-    def __init__(self, prog_mem, regs=None, inputf=std_in, outputf=std_out):
+    def __init__(self, prog_mem, regs=None, inputf=std_in, outputf=std_out, machine_id=None):
+        self.machine_id = machine_id
         self.prog_mem = prog_mem
         self.regs = regs
         if regs is None:
@@ -199,28 +247,44 @@ class Machine:
         return self.regs["pc"]
 
 
-def run(machine: Machine):
+def run(machine: Machine, should_print_state=False):
     while machine.get_pc() >= 0:
+        if should_print_state:
+            print_state(machine.prog_mem, machine.regs)
         tick(machine.prog_mem, machine.regs, machine.inputf, machine.outputf)
+    if should_print_state:
+        print_state(machine.prog_mem, machine.regs)
 
 
-def run_concurrent(*machines: Machine):
-    machines_enumerated: {[int,(Machine, Status)]} = dict(
-        map(lambda item: (item[0], item[1]), enumerate(machines)))
+def run_concurrent(*machines: Machine, should_print_state=False):
+    machine_id = 0
+    machines_enumerated: {str: (Machine, Status)} = dict()
+    for machine in machines:
+        if machine.machine_id is None:
+            while machine_id in machines_enumerated:
+                machine_id += 1
+            machine.machine_id = machine_id
+            machine_id += 1
+        if machine.machine_id in machines_enumerated:
+            raise Exception("machine_id {} is not unique".format(machine.machine_id))
+        machines_enumerated[machine.machine_id] = machine
+        
     while len(machines_enumerated) > 0:
         for (i, machine) in list(machines_enumerated.items()):
             if machine.get_pc() >= 0:
                 prev_pc = machine.get_pc()
-                print("Machine {}".format(i))
-                print_state(machine.prog_mem, machine.regs)
+                if should_print_state:
+                    print("Machine {}[{}]".format(i, machine.status))
+                    print_state(machine.prog_mem, machine.regs)
                 tick(machine.prog_mem, machine.regs, lambda: machine.inputf(i), lambda out: machine.outputf(out, i))
                 if prev_pc == machine.get_pc():
                     machine.status = Status.WAITING
                 else:
                     machine.status = Status.RUNNING
             else:
-                print("Machine {}".format(i))
-                print_state(machine.prog_mem, machine.regs)
+                if should_print_state:
+                    print("Machine {}".format(i))
+                    print_state(machine.prog_mem, machine.regs)
                 machine.status = Status.TERMINATED
                 machines_enumerated.pop(i)
 
